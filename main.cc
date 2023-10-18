de0ae768af15c43b4efd7b7ba95708b11c0533ee
Lolitron <Ovcharov_05@mail.ru>
2023-10-18 22:34:35 +0300

    db test

--- main.cc
+++ main.cc
@@ -1,9 +1,34 @@
 #include <drogon/drogon.h>
+#include <drogon/orm/Mapper.h>
+#include <models/Test.h>
+
 int main() {
+    drogon::HttpAppFramework::instance().registerHandler("/", [](const drogon::HttpRequestPtr& req,
+        std::function<void(const drogon::HttpResponsePtr&)>&& callback)
+        {
+            static int count = 0;
+
+            auto dbClientPtr = drogon::app().getDbClient();
+            drogon::orm::Mapper<drogon_model::testdb::Test> mapper{ dbClientPtr };
+
+
+            auto result = mapper.findFutureAll().get();
+            Json::Value json, objJson;
+            json["objects"] = {};
+            for (const auto& obj : result)
+            {
+                objJson["name"] = obj.getValueOfName();
+                objJson["number"] = obj.getValueOfNumber();
+                json["objects"].append(objJson);
+            }
+
+            callback(drogon::HttpResponse::newHttpJsonResponse(json));
+        });
+
     //Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0",80);
     //Load config file
-    //drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile("../config.json");
     //drogon::app().loadConfigFile("../config.yaml");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
