495a1c4335b60c13e981e59b6aa742c56673835a
unknown <ovcharov_05@mail.ru>
2023-10-19 17:50:02 +0300

    libsodium

--- main.cc
+++ main.cc
@@ -1,7 +1,7 @@
 #include <drogon/drogon.h>
 #include <drogon/orm/Mapper.h>
 #include <models/Test.h>
-#include <bcrypt/bcrypt.h>
+#include <sodium.h>
 
 int main() {
     drogon::HttpAppFramework::instance().registerHandler("/", [](const drogon::HttpRequestPtr& req,
@@ -26,14 +26,33 @@ int main() {
             callback(drogon::HttpResponse::newHttpJsonResponse(json));
         });
 
-    char salt[BCRYPT_HASHSIZE];
-    char hash[BCRYPT_HASHSIZE];
-    bcrypt_gensalt(12, salt);
-    bcrypt_hashpw("12345", salt, hash);
-    std::cout << hash << '\n';
+    if (sodium_init() < 0)
+    {
+        std::cerr << "Failed to load sodium!";
+        abort();
+    }
+
+    std::string pass = "123456";
+    char hashed_password[crypto_pwhash_STRBYTES];
+
+    if (crypto_pwhash_str
+    (hashed_password, pass.c_str(), pass.length(),
+        crypto_pwhash_OPSLIMIT_MODERATE, crypto_pwhash_MEMLIMIT_INTERACTIVE) != 0) { //testing only
+        std::cerr << "out of memory!\n";
+    }
+    std::cout << hashed_password << '\n';
+
+    if (crypto_pwhash_str_verify(hashed_password, "123456", 6) != 0)
+    {
+        std::cout << "wrong\n";
+    }
+    else
+    {
+        std::cout << "correct\n";
+    }
 
     //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0",80);
+    drogon::app().addListener("0.0.0.0", 80);
     //Load config file
     drogon::app().loadConfigFile("../config.json");
     //drogon::app().loadConfigFile("../config.yaml");
